3c752a831574c3c60c2d0872791d1df5dfc1e2df
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-06-09 11:29:59 +0300

    Add files via upload

---
+++ PROGRESS/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PROGRESS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.PercentageLbl = new System.Windows.Forms.Label();
+            this.RollingPrb = new System.Windows.Forms.ProgressBar();
+            this.MyTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(423, 157);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // PercentageLbl
+            // 
+            this.PercentageLbl.AutoSize = true;
+            this.PercentageLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.PercentageLbl.Location = new System.Drawing.Point(205, 89);
+            this.PercentageLbl.Name = "PercentageLbl";
+            this.PercentageLbl.Size = new System.Drawing.Size(63, 37);
+            this.PercentageLbl.TabIndex = 1;
+            this.PercentageLbl.Text = "0%";
+            // 
+            // RollingPrb
+            // 
+            this.RollingPrb.Location = new System.Drawing.Point(85, 157);
+            this.RollingPrb.Name = "RollingPrb";
+            this.RollingPrb.Size = new System.Drawing.Size(308, 23);
+            this.RollingPrb.TabIndex = 2;
+            // 
+            // MyTimer
+            // 
+            this.MyTimer.Tick += new System.EventHandler(this.MyTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 365);
+            this.Controls.Add(this.RollingPrb);
+            this.Controls.Add(this.PercentageLbl);
+            this.Controls.Add(this.StartBtn);
+            this.Name = "MainForm";
+            this.Text = "Progress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label PercentageLbl;
+        private System.Windows.Forms.ProgressBar RollingPrb;
+        private System.Windows.Forms.Timer MyTimer;
+    }
+}
+
---
+++ PROGRESS/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace PROGRESS
+{
+    public partial class MainForm : Form
+    {
+        int num = 0;
+        const int MAX = 100;
+        const int STEPS = 1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            this.MyTimer.Start();
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            this.RollingPrb.Increment(STEPS);
+            num = num + STEPS;
+            PercentageLbl.Text = num.ToString() + "%";
+            if (num > MAX)
+            {
+                this.MyTimer.Stop();
+                PercentageLbl.Text = "100%";
+            }
+        }
+    }
+}
